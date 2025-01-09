4450a5c1087dd162d7ba4f7e6105b86948bf65a2
Rikesh Shrestha <rikesh.shrestha.npl@gmail.com>
2025-01-09 17:59:18 +0700

    feat: Add service

---
+++ src/common/services/book-keeping.ts
@@ -0,0 +1,40 @@
+import { Employee } from '../models/employee';
+import { EmployeeRepoInterface } from '../repo/contracts';
+
+const RELEASE_TYPE_MONTHLY = 'MONTHLY';
+const RELEASE_TYPE_DAILY = 'DAILY';
+
+
+export class BookkeepingService {
+  #employeeRepo: EmployeeRepoInterface;
+
+  constructor(employeeRepo: EmployeeRepoInterface) {
+    this.#employeeRepo = employeeRepo;
+  }
+
+
+  _calculateSalary(releaseType: string, salary: number): number {
+    switch (releaseType) {
+      case RELEASE_TYPE_MONTHLY:
+        return Math.round(salary / 30);
+      case RELEASE_TYPE_DAILY:
+        return salary;
+      default:
+        throw new Error(`Unsupported release type: ${releaseType}`);
+    }
+  }
+
+  updateEmployeeBalance(employee: Employee): Promise<void> {
+    const calculator = this._calculateSalary(employee.releaseType, employee.salary);
+
+    return this._makeEntry(employee, calculator);
+  }
+
+  _makeEntry(employee: Employee, dailySalary: number): Promise<void> {
+    return this.#employeeRepo
+      .updateBalance(employee.id, dailySalary)
+      .then(() => {
+        console.log("Salary balance updated", { "employeeId": employee.id });
+      });
+  }
+};
