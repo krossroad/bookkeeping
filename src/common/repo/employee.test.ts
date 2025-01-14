002e2bce7bfcea5910de1a8c837a1fa0b30f9abe
Rikesh Shrestha <rikesh.shrestha.npl@gmail.com>
2025-01-14 09:24:16 +0700

    feat: Add test for repo

---
+++ src/common/repo/employee.test.ts
@@ -0,0 +1,93 @@
+import { PrismaClient } from "@prisma/client";
+import { EmployeeRepo } from "./employee";
+import { Employee } from "../models/employee";
+import { describe, it, expect } from "@jest/globals";
+
+jest.mock("@prisma/client");
+
+describe("EmployeeRepo", () => {
+  let prismaClient: PrismaClient;
+  let employeeRepo: EmployeeRepo;
+
+  beforeEach(() => {
+    prismaClient = {
+      employee: {
+        update: jest.fn(),
+      },
+    } as unknown as PrismaClient;
+    employeeRepo = new EmployeeRepo(prismaClient);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  const now = new Date();
+
+  describe("updateBalance", () => {
+    it("should update the employee balance and return the updated employee", async () => {
+      const employeeId = "1";
+      const amount = 100;
+      const updatedEmployee: Employee = {
+        id: employeeId,
+        salaryBalance: 200,
+        salaryAccountedTill: now,
+        firstName: "",
+        lastName: "",
+        email: "",
+        salary: 0,
+        companyId: "",
+        idType: "PASSPORT",
+        idNumber: "",
+        releaseType: "MONTHLY",
+        createdAt: now,
+        updatedAt: now,
+        deletedAt: null,
+      };
+
+      prismaClient.employee.update = jest
+        .fn()
+        .mockResolvedValue(updatedEmployee);
+
+      const result = await employeeRepo.updateBalance(employeeId, amount);
+
+      expect(prismaClient.employee.update).toHaveBeenCalledWith({
+        where: {
+          id: employeeId,
+          salaryAccountedTill: {
+            lt: expect.any(Date),
+          },
+        },
+        data: {
+          salaryBalance: { increment: amount },
+          salaryAccountedTill: expect.any(Date),
+        },
+      });
+      expect(result).toEqual(updatedEmployee);
+    });
+
+    it("should return null if there is an error updating the employee balance", async () => {
+      const employeeId = "1";
+      const amount = 100;
+
+      prismaClient.employee.update = jest
+        .fn()
+        .mockRejectedValue(new Error("Update error"));
+
+      const result = await employeeRepo.updateBalance(employeeId, amount);
+
+      expect(prismaClient.employee.update).toHaveBeenCalledWith({
+        where: {
+          id: employeeId,
+          salaryAccountedTill: {
+            lt: expect.any(Date),
+          },
+        },
+        data: {
+          salaryBalance: { increment: amount },
+          salaryAccountedTill: expect.any(Date),
+        },
+      });
+      expect(result).toBeNull();
+    });
+  });
+});
